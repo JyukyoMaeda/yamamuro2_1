3c4d8037a20075f7c4a2d7ad4ca16931fcf73713
JyukyoMaeda <maeda-r@jyukyo.com>
2023-05-25 19:03:07 +0900

    サイト作成 > 準備

---
+++ 元データ/js/pagetop.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+
+
+
+jQuery(function() {
+  var appear = false;
+  var pagetop = $('#page_top');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {  //100pxスクロールしたら
+      if (appear == false) {
+        appear = true;
+        pagetop.stop().animate({
+          'right': '10px' //右から0pxの位置に
+        }, 300); //0.3秒かけて現れる
+      }
+    } else {
+      if (appear) {
+        appear = false;
+        pagetop.stop().animate({
+          'right': '-50px' //右から-50pxの位置に
+        }, 300); //0.3秒かけて隠れる
+      }
+    }
+  });
+});
