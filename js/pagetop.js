aa2fef70730066122b596b2ec3ff80838e6a5b9d
JyukyoMaeda <maeda-r@jyukyo.com>
2023-05-26 09:18:09 +0900

    サイト構造 > 修正

--- js/pagetop.js
+++ js/pagetop.js
@@ -1,42 +1,36 @@
 // JavaScript Document
-//スクロールした際の動きを関数でまとめる
-function PageTopAnime() {
-   var scroll = $(window).scrollTop();
-   if (scroll >= 1500){
-         $('#pageTop').removeClass('DownMove');
-         $('#pageTop').addClass('UpMove');
-   }else{
-         if($('#pageTop').hasClass('UpMove')){
-         $('#pageTop').removeClass('UpMove');
-         $('#pageTop').addClass('DownMove');
-         }
-   }
-   }
-   
-   // 画面をスクロールをしたら動かしたい場合の記述
-   $(window).scroll(function () {
-         PageTopAnime();
-   });
-   
-   // ページが読み込まれたらすぐに動かしたい場合の記述
-   $(window).on('load', function () {
-         PageTopAnime();
-   });
-   
-   
-   $(function(){
-      // #で始まるアンカーをクリックした場合に処理
-      $('a[href^="#"]:not(.inline)').click(function() {
-         // スクロールの速度
-         var speed = 300; // ミリ秒
-          // 移動先を取得
-         var href= $(this).attr("href");
-         var target = $(href == "#" || href == "" ? 'html' : href);
-         // 移動先を数値で取得
-         var position = target.offset().top;
-         // スムーススクロール
-         $('body,html').animate({scrollTop:position}, speed, 'swing');
-         return false;
-      });
-   });
-   
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+
+
+
+jQuery(function() {
+  var appear = false;
+  var pagetop = $('#page_top');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {  //100pxスクロールしたら
+      if (appear == false) {
+        appear = true;
+        pagetop.stop().animate({
+          'right': '10px' //右から0pxの位置に
+        }, 300); //0.3秒かけて現れる
+      }
+    } else {
+      if (appear) {
+        appear = false;
+        pagetop.stop().animate({
+          'right': '-50px' //右から-50pxの位置に
+        }, 300); //0.3秒かけて隠れる
+      }
+    }
+  });
+});
